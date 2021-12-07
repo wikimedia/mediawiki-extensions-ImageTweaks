0d3834eb9846266d5b42ebd471be30030ee556b9
Umherirrender <umherirrender_de.wp@web.de>
2021-12-07 12:39:14 +0100

    Emit deprecation warning from php entry point
    Change-Id: I431998f6ec36351871a71fefeb2a074124761740

--- ImageTweaks.php
+++ ImageTweaks.php
@@ -14,8 +14,11 @@
 
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'ImageTweaks' );
-
+	wfWarn(
+		'Deprecated PHP entry point used for ImageTweaks extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
 	return true;
 }
 
-die( 'This version of the ImageTweaks extension requires MediaWiki 1.25+.' );
+die( 'This version of the ImageTweaks extension requires MediaWiki 1.36+.' );
